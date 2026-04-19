2571da6a4687fcf14127dc9ec69f42162ba582db
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-04-19 16:29:06 +0700

    feat(be): got inference working

--- apps/backend/chat/chat.controller.ts
+++ apps/backend/chat/chat.controller.ts
@@ -1,14 +1,13 @@
 import { api, StreamInOut } from "encore.dev/api";
 import { Message } from "./message.interface";
 import log from "encore.dev/log";
+import claude from "./claude";
 
-// Map to hold all connected streams
 const connectedStreams: Map<
   string,
   StreamInOut<Message, Message>
 > = new Map();
 
-// Object sent from the client to the server when establishing a connection
 interface HandshakeRequest {
   id: string;
 }
@@ -18,27 +17,23 @@ export const chat = api.streamInOut<HandshakeRequest, Message, Message>(
   async (handshake, stream) => {
     connectedStreams.set(handshake.id, stream);
 
+    log.info("Stream connected:", stream);
     try {
       // The stream object is an AsyncIterator that yields incoming messages.
       // The loop will continue as long as the client keeps the connection open.
       for await (const chatMessage of stream) {
         for (const [key, val] of connectedStreams) {
           try {
-            // Send the users message to all connected clients.
-            log.trace("Incoming:", chatMessage);
-            await val.send(chatMessage);
+            const res = await claude.invoke(chatMessage.content);
+            await val.send({ ...chatMessage, sender: "assistant", content: res.content.toString(), timestamp: new Date() });
           } catch (err) {
-            // If there is an error sending the message, remove the client from the map.
             connectedStreams.delete(key);
           }
         }
       }
     } catch (err) {
-      // If there is an error reading from the stream, remove the client from the map.
       connectedStreams.delete(handshake.id);
     }
-
-    // When the client disconnects, remove them from the map.
     connectedStreams.delete(handshake.id);
   },
 );
--- apps/backend/chat/message.interface.ts
+++ apps/backend/chat/message.interface.ts
@@ -1,6 +1,10 @@
 export interface Message {
+  id: string;
+  userId: number;
+  userName: string;
   sender: "user" | "assistant";
   content: string;
+  timestamp: Date;
 }
 
 export interface InMessage extends Omit<Message, "timestamp"> { sender: "user"; };
