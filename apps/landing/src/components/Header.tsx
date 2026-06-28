8613a91d0d5dbcf4a3fcd96531cf62a62a2e5969
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-28 16:01:43 +0700

    feat(landing): responsive nav

--- apps/landing/src/components/Header.tsx
+++ apps/landing/src/components/Header.tsx
@@ -1,32 +1,37 @@
+import { MobileNav } from './MobileNav';
 import { Button } from './ui/button';
 
 export default function Header() {
   return (
     <header className="fixed z-50 top-0 right-auto left-auto flex w-full p-4 max-w-4xl rounded-[12px] justify-between items-center backdrop-blur-sm bg-white/50 border border-gray-100/50">
       <img src="/walker.svg" alt="Walker Logo" className="h-6" />
-      <div className="flex gap-3">
-        <a href="https://walker.vstaffs.com/docs">
+      <div className="flex items-center">
+        <div className="hidden md:flex gap-3">
+          <a href="https://walker.vstaffs.com/docs">
+            <Button variant="ghost" size="lg" className="rounded-none">
+              Docs
+            </Button>
+          </a>
+          <a href="https://github.com/lekhoinguyen06/walker">
+            <Button variant="ghost" size="lg" className="rounded-none">
+              Github
+            </Button>
+          </a>
           <Button variant="ghost" size="lg" className="rounded-none">
-            Docs
+            Community
           </Button>
-        </a>
-        <a href="https://github.com/lekhoinguyen06/walker">
-          <Button variant="ghost" size="lg" className="rounded-none">
-            Github
-          </Button>
-        </a>
-        <Button variant="ghost" size="lg" className="rounded-none">
-          Community
-        </Button>
-        <a href="https://concierge.vstaffs.com">
-          <Button
-            variant="ghost"
-            size="lg"
-            className="rounded-none hover:bg-red-50"
-          >
-            Try <img src="/concierge-inline.svg" alt="Concierge Inline Logo" />
-          </Button>
-        </a>
+          <a href="https://concierge.vstaffs.com">
+            <Button
+              variant="ghost"
+              size="lg"
+              className="rounded-none hover:bg-red-50"
+            >
+              Try{' '}
+              <img src="/concierge-inline.svg" alt="Concierge Inline Logo" />
+            </Button>
+          </a>
+        </div>
+        <MobileNav />
       </div>
     </header>
   );
---
+++ apps/landing/src/components/MobileNav.tsx
@@ -0,0 +1,78 @@
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { PanelRight } from 'lucide-react';
+
+export function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon-lg"
+          className="rounded-none md:hidden"
+        >
+          <PanelRight />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle className="font-brand text-2xl">Navigation</SheetTitle>
+          <SheetDescription>Let's take a walk!</SheetDescription>
+        </SheetHeader>
+        <div className="flex flex-col">
+          <a href="https://concierge.vstaffs.com">
+            <Button
+              variant="ghost"
+              size="lg"
+              className="rounded-none w-full justify-start py-12 hover:bg-red-50"
+            >
+              Try{' '}
+              <img src="/concierge-inline.svg" alt="Concierge Inline Logo" />
+            </Button>
+          </a>
+          <a href="https://walker.vstaffs.com/docs">
+            <Button
+              variant="ghost"
+              size="lg"
+              className="rounded-none w-full justify-start py-12"
+            >
+              Docs
+            </Button>
+          </a>
+          <a href="https://github.com/lekhoinguyen06/walker">
+            <Button
+              variant="ghost"
+              size="lg"
+              className="rounded-none w-full justify-start py-12"
+            >
+              Github
+            </Button>
+          </a>
+          <Button
+            variant="ghost"
+            size="lg"
+            className="rounded-none w-full justify-start py-12"
+          >
+            Community
+          </Button>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button variant="ghost" className="w-full rounded-none py-12">
+              Close
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
