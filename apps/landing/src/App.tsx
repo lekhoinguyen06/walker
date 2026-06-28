b34817d66ed92af87bdc5357e5e3cb4823ebe081
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-28 13:21:03 +0700

    feat(landing): add slug

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -1,9 +1,28 @@
 import Header from './components/Header';
+import ReactLogo from './assets/react.svg?react';
+import TypescriptLogo from './assets/typescript.svg?react';
 
 function App() {
   return (
     <div className="flex flex-col gap-12 items-center">
       <Header />
+
+      <div className="relative flex flex-col w-full justify-end items-center">
+        <div className="relative size-full min-h-180 flex flex-col justify-end items-center">
+          <img
+            src="/yellow.svg"
+            alt="Yellow Background"
+            className="absolute top-0 left-0 size-full"
+          />
+          <div className="flex flex-col gap-12 items-center justify-center mb-24 z-1">
+            <div className="font-brand">Walk the web, walk the earth.</div>
+            <div className="flex gap-12 items-center">
+              <ReactLogo className="h-6 fill-[#61DAFB]" />
+              <TypescriptLogo className="h-6 fill-[#3178C6]" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -3,12 +3,14 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    svgr(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
   resolve: {
