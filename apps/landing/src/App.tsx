8748ea282aefcad0ad43532f85e23e326df185e4
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-28 15:50:13 +0700

    feat(landing): responsive body

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -8,88 +8,89 @@ import YellowLightBackground from './assets/yellow-light.svg?react';
 
 function App() {
   return (
-    <div className="absolute w-full flex flex-col gap-48 items-center">
-      <Header />
+    <div className="w-full flex flex-col gap-48 items-center bg-background">
+      <div className=" w-full flex flex-col lg:gap-48 gap-24 items-center max-w-6xl px-6">
+        <Header />
 
-      {/* Hero Section */}
-      <div className="relative size-full min-h-180 max-w-6xl flex flex-col justify-end items-center mt-24">
-        <YellowBackground
-          className="absolute inset-0 size-full object-fill"
-          preserveAspectRatio="none"
-        />
-        <div className="flex flex-col gap-6 items-center justify-center mb-24 z-1">
-          <div className="font-brand">Walk the web, walk the earth.</div>
-          <div className="flex gap-6 items-center">
-            <ReactLogo className="h-6 fill-[#61DAFB]" />
-            <TypescriptLogo className="h-6 fill-[#3178C6]" />
+        {/* Hero Section */}
+        <div className="relative size-full min-h-180 flex flex-col justify-end items-center mt-24">
+          <YellowBackground
+            className="absolute inset-0 size-full object-fill"
+            preserveAspectRatio="none"
+          />
+          <div className="flex flex-col gap-6 items-center justify-center mb-24 z-1">
+            <div className="font-brand">Walk the web, walk the earth.</div>
+            <div className="flex gap-6 items-center">
+              <ReactLogo className="h-6 fill-[#61DAFB]" />
+              <TypescriptLogo className="h-6 fill-[#3178C6]" />
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Sponsors */}
-      <div className="relative size-full min-h-140 max-w-6xl flex flex-col justify-center items-center">
-        <YellowLightBackground
-          className="absolute inset-0 size-full object-fill"
-          preserveAspectRatio="none"
-        />
-        <img src="/vstaffs.svg" alt="Vstaffs Logo" className="h-6 z-1" />
-      </div>
+        {/* Sponsors */}
+        <div className="relative size-full min-h-140 flex flex-col justify-center items-center">
+          <YellowLightBackground
+            className="absolute inset-0 size-full object-fill"
+            preserveAspectRatio="none"
+          />
+          <img src="/vstaffs.svg" alt="Vstaffs Logo" className="h-6 z-1" />
+        </div>
 
-      {/* Merchandise */}
-      <div className="flex flex-col gap-12 w-full max-w-6xl ">
-        <div className="font-brand text-2xl">Merchandise</div>
-        <div className="p-2 h-120 flex gap-6 overflow-x-scroll no-scrollbar">
-          <Merchandise
-            title="T Shirt"
-            description="A comfortable cotton t-shirt with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="Shorts"
-            description="A comfortable pair of shorts with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="Cup"
-            description="A stylish cup with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="Hoodie"
-            description="A cozy hoodie with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="Sticker"
-            description="A fun sticker with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="NFT"
-            description="A unique digital collectible with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="Hat"
-            description="A stylish hat with the Walker logo."
-            image="/image.png"
-          />
-          <Merchandise
-            title="Socks"
-            description="A comfortable pair of socks with the Walker logo."
-            image="/image.png"
-          />
+        {/* Merchandise */}
+        <div className="flex flex-col gap-12 w-full ">
+          <div className="font-brand text-2xl">Merchandise</div>
+          <div className="p-2 h-120 flex gap-6 overflow-x-scroll no-scrollbar">
+            <Merchandise
+              title="T Shirt"
+              description="A comfortable cotton t-shirt with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="Shorts"
+              description="A comfortable pair of shorts with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="Cup"
+              description="A stylish cup with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="Hoodie"
+              description="A cozy hoodie with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="Sticker"
+              description="A fun sticker with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="NFT"
+              description="A unique digital collectible with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="Hat"
+              description="A stylish hat with the Walker logo."
+              image="/image.png"
+            />
+            <Merchandise
+              title="Socks"
+              description="A comfortable pair of socks with the Walker logo."
+              image="/image.png"
+            />
+          </div>
+        </div>
+
+        {/* Spreadsheet */}
+        <div className="flex flex-col gap-12 w-full">
+          <div className="font-brand text-2xl">Spreadsheet</div>
+          <SpreadSheet />
         </div>
       </div>
-
-      {/* Spreadsheet */}
-      <div className="flex flex-col gap-12 w-full max-w-6xl">
-        <div className="font-brand text-2xl">Spreadsheet</div>
-        <SpreadSheet />
-      </div>
-
       {/* Footer */}
-      <div className="w-full min-h-140 flex flex-col justify-end items-center bg-foreground">
+      <div className="w-full min-h-60 lg:min-h-120 flex flex-col justify-end items-center bg-foreground">
         <div className="font-brand text-white">MIT License 2026</div>
       </div>
     </div>
