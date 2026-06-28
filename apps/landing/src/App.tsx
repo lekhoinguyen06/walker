aa56cc50f8376204a876c3de257de6d37862d9a8
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-28 13:41:36 +0700

    feat(landing): add links & footer

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -4,10 +4,11 @@ import TypescriptLogo from './assets/typescript.svg?react';
 
 function App() {
   return (
-    <div className="absolute w-full flex flex-col gap-12 items-center">
+    <div className="absolute w-full flex flex-col gap-24 items-center">
       <Header />
 
-      <div className="relative size-full min-h-180 flex flex-col justify-end items-center">
+      {/* Hero Section */}
+      <div className="relative size-full min-h-180 flex flex-col justify-end items-center mt-24">
         <img
           src="/yellow.svg"
           alt="Yellow Background"
@@ -22,6 +23,7 @@ function App() {
         </div>
       </div>
 
+      {/* Sponsors */}
       <div className="relative size-full min-h-140 flex flex-col justify-center items-center">
         <img
           src="/yellow-light.svg"
@@ -29,6 +31,14 @@ function App() {
           className="absolute top-0 left-0 size-full"
         />
         <img src="/vstaffs.svg" alt="Vstaffs Logo" className="h-6 z-1" />
+      </div>
+
+      {/* Footer */}
+      <div className="w-full min-h-140 flex flex-col justify-between items-center px-24 bg-foreground">
+        <div className="w-full">
+          <img src="/walker-light.svg" alt="Walker Logo" className="h-6" />
+        </div>
+        <div className="font-brand text-white">MIT License 2026</div>
       </div>
     </div>
   );
