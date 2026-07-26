d7e59e329f0cbc8230a0fe8a91917d1483fe58e2
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-07-26 14:06:22 +0700

    feat(landing/react): add Input example

--- apps/playground-react/src/AppLayout.tsx
+++ apps/playground-react/src/AppLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header";
+import { Toaster } from "./components/ui/toast";
 
 function AppLayout() {
   return (
     <div className="w-full min-h-screen justify-center flex flex-col gap-24 p-24 items-center overflow-y-scroll">
       <Header />
       <Outlet />
+      <Toaster />
     </div>
   );
 }
